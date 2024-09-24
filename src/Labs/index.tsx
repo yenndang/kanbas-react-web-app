9599444cef2228c2ec6304ad34dcb40d9b810114
yenlnldang@gmail.com <yenlnldang@gmail.com>
2024-09-23 21:06:42 -0400

    A1 - add Kanbas link to lab page for navigation

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Kanbas from "../Kanbas";
 export default function Labs() {
   return (
     <div>
@@ -24,6 +25,7 @@ export default function Labs() {
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
+        <Route path="Kanbas" element={<Kanbas />} />
       </Routes>
     </div>
   );
